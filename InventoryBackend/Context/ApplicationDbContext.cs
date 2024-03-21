2cbc8e7129ca0126320afb99bec0274da113d999
JoelVR17 <vermudo.com@gmail.com>
2024-03-21 16:40:14 -0600

    API Rest of Products and Categories are done

---
+++ InventoryBackend/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using InventoryBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryBackend.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { 
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ InventoryBackend/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryBackend.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
